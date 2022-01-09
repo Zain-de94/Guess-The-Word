d995028d9ccafbf331eed3924b9a62b7a51465f9
Zain-de94 <71317396+Zain-de94@users.noreply.github.com>
2022-01-09 14:26:16 +0530

    Step-3-Add-Live-data-to-the-viewmodel

---
+++ Guesstheword/app/src/main/java/com/example/guesstheword/screens/game/GameFragment.kt
@@ -0,0 +1,68 @@
+package com.example.guesstheword.screens.game
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.NavHostFragment.findNavController
+import com.example.guesstheword.R
+import com.example.guesstheword.databinding.FragmentGameBinding
+
+
+class GameFragment : Fragment() {
+
+    private lateinit var viewModel: GameViewModel
+
+    private lateinit var binding: FragmentGameBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate view and obtain an instance of the binding class
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_game,
+            container,
+            false
+        )
+
+        viewModel = ViewModelProvider(this).get(GameViewModel::class.java)
+
+
+        binding.correctButton.setOnClickListener {
+            viewModel.onCorrect()
+        }
+
+        binding.skipButton.setOnClickListener {
+            viewModel.onSkip()
+        }
+
+        viewModel.word.observe(viewLifecycleOwner, { newWord ->
+
+            binding.wordText.text = newWord
+
+        })
+
+        viewModel.score.observe(viewLifecycleOwner, { newScore ->
+            binding.scoreText.text = newScore.toString()
+        })
+
+        return binding.root
+    }
+
+
+    /**
+     * Called when the game is finished
+     */
+    private fun gameFinished() {
+        val currentScore = viewModel.score.value ?: 0
+        val action = GameFragmentDirections.actionGameToScore(currentScore)
+        findNavController(this).navigate(action)
+    }
+
+
+}
